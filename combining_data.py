bb67eec4ba3b6d376d28ae78439c0f5f74613fe4
Lizzie Healy <emh201@georgetown.edu>
2024-12-03 16:25:41 -0500

    combining the datasets

---
+++ combining_data.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import os
+import json
+
+output_path = "output_data/"
+all_files = [f for f in os.listdir(output_path) if f.endswith('.csv')]
+genre_df = pd.concat([pd.read_csv(os.path.join(output_path, file)) for file in all_files], ignore_index=True)
+    
+movie_list = []
+for filename in os.listdir('data'):
+    input_file = os.path.join('Data', filename)
+        
+    with open(input_file, 'r', encoding='Windows-1252') as file:
+        movie_data = json.load(file)
+        movie_list.append(pd.DataFrame(movie_data))
+movie_df = pd.concat(movie_list, ignore_index=True)
+
+# print the unique
+print(len(genre_df))
+print(genre_df['movie'].nunique())
+print(len(movie_df))
+print(movie_df['Title'].nunique())
+
+# changing the movie variable name
+genre_df.rename(columns={'movie': 'Title'}, inplace=True)
+
+# merging
+merged_df = pd.merge(movie_df, genre_df, on='Title', how='inner')
+print(merged_df.head(10))
+print(len(merged_df))
+
+# keeping only unique titles
+merged_df = merged_df.drop_duplicates(subset=['Title'], keep='first')
+print(merged_df.head(10))
+print(len(merged_df))
+
